d7782cd9ae019b00e96a568694e29a9cdd77ba47
darshitkansara94 <darshitkansara94@gmail.com>
2023-12-08 20:08:11 +0530

    Date : 07-12-2023

--- JS/errorhandling.js
+++ JS/errorhandling.js
@@ -21,12 +21,27 @@
 //     // Process to be done if code is errored or not.
 // }
 
+console.log("Error Handling JS");
 
+// All the logic of your code will be add in try block.
+// If any error occur in try block then pointer move to the catch part and display error message if we declare a variable.
 try {
-    const day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Firday", "Saturday"];
+    const abc = "test";
 
-    let dayArray = day[9];
+    if(abc == ""){
+        // It throw error and debug move to the catch block and display message if given.
+        // We can use throw in all the three block.
+        throw new Error("abc can not be blank");
+    }
+    // function
+    function erroredfunction(){}
+    erroredfunction();
 }
-catch (errorMessage) {
-    alert(errorMessage);
+// catch block execute when any error found in try block /  section.
+catch (ten) { // ten = Variable name
+    alert(ten);
+}
+// Finally block is execute in all the case if code is error out or not.
+finally{
+    alert("Execute in all situation");
 }
